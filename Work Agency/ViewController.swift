4a63cf61a64d70b16302d2e09478bbcc4060a849
Ivan Ermak <ermak.ivan.iv@gmail.com>
2019-09-24 13:57:54 +0300

    Initial Commit

---
+++ Work Agency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Work Agency
+//
+//  Created by Ivan Ermak on 9/24/19.
+//  Copyright © 2019 Ivan Ermak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
